fad57a1359e9c6ef7c2319699b51abd85c34fce9
jotinhaxit <127160960+jotinhaxit@users.noreply.github.com>
2024-05-15 20:04:19 -0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function toggleImage() {
+    var img = document.getElementById("imagem-impacto");
+    if (img.style.display === "none") {
+        img.style.display = "block";
+    } else {
+        img.style.display = "none";
+    }
+}
